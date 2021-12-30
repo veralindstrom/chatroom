693b7b80e957a6ec5db21294a703e519387fef66
Vera Lindström <veraalindss@gmail.com>
2021-12-30 13:27:55 +0100

    inital commit

---
+++ chatroom_project/src/test/java/kth/id1212/project/ChatroomApplicationTests.java
@@ -0,0 +1,13 @@
+package kth.id1212.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
